83aa8d46b85eef3dd213bee3b9d8468e22305907
Stefan Henneken <stefan.henneken@live.com>
2017-06-03 23:14:25 +0200

    Add project files.

---
+++ CarMercedes/Mercedes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using CarContract;
+
+namespace CarMercedes
+{ 
+    [Export(typeof(ICarContract))]
+    public class Mercedes : ICarContract
+    {
+        private Mercedes()
+        {
+            Console.WriteLine("Mercedes constructor.");
+        }
+        public string StartEngine(string name)
+        {
+            return String.Format("{0} starts the Mercedes.", name);
+        }
+    }
+}
